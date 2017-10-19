ae007ec073623456c492e144041cc9c81835d342
Bruno Jobard <bruno.jobard@univ-pau.fr>
2017-10-19 19:13:53 +0200

    Rocket SC

--- src/Rocket_SC/rocket.js
+++ src/Rocket_SC/rocket.js
@@ -1,103 +1,122 @@
 
 
-var model = {
-  id: 'rocketLaunch',
-  states: [
-    {
-      id: 'ready',
-      transitions: [
-        {
-          target: 'counting',
-          event: 'click'
-        }
-      ]
-    }, {
-      id: 'counting',
-      onEntry: function(event) {actions.counting(event)},
-      transitions: [
-        {
-          target: 'aborted',
-          event: 'abortClick'
-        }, {
-          target: 'launched',
-          event: 'timerEnd'
-        }
-      ]
-    }, {
-      id: 'aborted',
-      transitions: [
-        {
-          target: 'ready',
-          event: 'restartClick'
-        }
-      ]
-    }, {
-      id: 'launched',
-      transitions: [
-        {
-          target: 'ready',
-          event: 'restartClick'
-        }
-      ]
-    }
-  ]
-};
+  ////////////////////////////////////////////////////////////////////////////////
+  // Model
+  //
+  const COUNTER_MAX = 10;
 
-////////////////////////////////////////////////////////////////////////////////
-// View
-//
-var view = {};
+  var model = {
+    counter: COUNTER_MAX,
+    started: false,
+    launched: false,
+    aborted: false
+  };
 
-// Initial state representation
-view.init = function(model) {
-  let representation = view.ready(model);
-  return representation;
-}
+  var stateMachine = {
+    states: [
+      {
+        id: 'ready',
+        transitions: [
+          {
+            target: 'counting',
+            event: 'clickStart'
+          }
+        ]
+      }, {
+        id: 'counting',
+        onEntry: function(event) {
+          actions.counting(event)
+        },
+        transitions: [
+          {
+            target: 'aborted',
+            event: 'abortClick'
+          }, {
+            target: 'launched',
+            event: 'timerEnd'
+          }
+        ]
+      }, {
+        id: 'aborted',
+        transitions: [
+          {
+            target: 'ready',
+            event: 'restartClick'
+          }
+        ]
+      }, {
+        id: 'launched',
+        transitions: [
+          {
+            target: 'ready',
+            event: 'restartClick'
+          }
+        ]
+      }
+    ]
+  };
 
-// State representation of the ready state
-view.ready = function(model) {
+  ////////////////////////////////////////////////////////////////////////////////
+  // View
+  //
+  var view = {};
 
-  let representation = `
+  // Initial state representation
+  view.init = function(model) {
+    let representation = view.ready(model);
+    return representation;
+  }
+
+  // State representation of the ready state
+  view.ready = function(model) {
+
+    let representation = `
     <p>Counter : ${model.counter}</p>
-    <input type="button" value="Start" onClick="actions.start({})">`;
+    <input type="button" value="Start" onClick="handleEvent('clickStart',model)">`;
 
-  return representation;
-}
+    return representation;
+  }
 
-// State representation of the counting state
-view.counting = function(model) {
+  // State representation of the counting state
+  view.counting = function(model) {
 
-  let representation = `
+    let representation = `
     <p>Count down: ${model.counter} </p>
 		<input type="button" value="Abort" onClick="actions.abort({})">`;
 
-  return representation;
-}
+    return representation;
+  }
 
-////////////////////////////////////////////////////////////////////////////////
-// Actions
-//
+  //display the state representation
+  view.display = function(representation) {
+    let stateRepresentation = document.getElementById("representation");
+    stateRepresentation.innerHTML = representation;
+  }
 
-var actions = {};
+  ////////////////////////////////////////////////////////////////////////////////
+  // Actions
+  //
 
-actions.counting = function (event) {
-  console.log("Counting ", event);
-}
+  var actions = {};
+
+  actions.counting = function(event) {
+    console.log("Counting ", event);
+  }
+
+  var sc = new scion.Statechart(stateMachine);
+  sc.start();
+
+  function handleEvent(type, data) {
+    sc.gen({name: type, data: data});
+  }
 
 window.addEventListener("load", function() {
 
-  var sc = new scion.Statechart(model);
-  sc.start();
+  // Display initial state
+  view.display(view.init(model));
 
-  function handleEvent(e) {
-    console.log(e.type);
-    e.preventDefault();
-    sc.gen({name: e.type, data: e});
-  }
+  //document.getElementById("start").addEventListener("click", handleEvent, true);
 
-  document.getElementById("start").addEventListener("click", handleEvent, true);
-
-  console.log(sc.getConfiguration());
   console.log(sc.getConfiguration());
 
 });
